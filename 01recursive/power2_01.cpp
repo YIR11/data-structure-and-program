9e42b2dde6a31c67999fcbdb1790a32eacd17298
guonz <guonzchn@163.com>
2020-05-16 15:17:55 +0800

    recursive

---
+++ 01recursive/power2_01.cpp
@@ -0,0 +1,15 @@
+//2^n
+#include <iostream>
+using namespace std;
+
+int power2(int n){
+	if (n==0){
+		return 1;
+	}
+	else{
+		return 2*power2(n-1);
+	}
+} 
+int main(){
+	cout<<power2(3)<<endl;
+}
